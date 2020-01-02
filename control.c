9546cfa2085f1de2f7c36db9468b6c931744b16c
Michael Robertson <mrobertson00@stuy.edu>
2020-01-01 23:48:32 -0500

    e

---
+++ control.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <errno.h>
+
+#define SEMKEY 24602
+#define SHMKEY 24601
+#define SEGSIZE 100
+
+union semun {
+  int              val;    /* Value for SETVAL */
+  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+  unsigned short  *array;  /* Array for GETALL, SETALL */
+  struct seminfo  *__buf;  /* Buffer for IPC_INFO */
+};
+
+int main (int argc, char *argv[]) {
+
+    //shared memory
+    int shmd;
+
+    //semaphore
+    int semd;
+    int v, r;
+
+    //file
+    int file;
+
+    if (argc == 1) {
+        printf("missing flag\n");
+        return 0;
+    }
+
+    else {
+        if (strcmp(argv[1],"-c") == 0) {
+            
+            //create semaphore
+            semd = semget(SEMKEY, 1, IPC_CREAT | IPC_EXCL | 0644);
+
+            if (semd == -1) {
+                printf("error %d: %s\n", errno, strerror(errno));
+                semd = semget(KEY, 1, 0);
+                v = semctl(semd, 0, GETVAL, 0);
+                printf("semctl returned: %d\n", v);
+            }
+  
+            else {
+                union semun us;
+                us.val = 1;
+                r = semctl(semd, 0, SETVAL, us);
+                printf("semctl returned: %d\n", r);
+            }
+
+            //create shared memory
+            shmd = shmget(KEY, SEG_SIZE, IPC_CREAT | 0644);
+
+            //create file
+            file = open("story", O_CREAT | O_EXCL | O_TRUNC | O_RDWR, 0644);
+        }
+
+        else if (strcmp(argv[1],"-v") == 0) {
+            printf("detects -v\n");
+        }
+
+        else if (strcmp(argv[1],"-r") == 0) {
+            
+            //display story
+
+            //remove semaphore
+            semctl(semd, IPC_RMID, 0);
+            printf("semaphore removed\n");
+
+            //remove shared memory
+            shmctl(shmd, IPC_RMID, 0);
+            printf("shared memory removed\n");
+
+            //remove file
+            remove("story");
+            printf("file removed\n");
+        }
+
+        else {
+            printf("invalid flag\n");
+            return 0;
+        }
+    }
+
+    return 0;
+}
---
+++ write.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <errno.h>
+
+#define SEMKEY 24602
+#define SHMKEY 24601
+#define SEGSIZE 100
+
+int main() {
+
+  int semd;
+  int r, v;
+  int file;
+  char * data;
+
+  semd = semget(SEMKEY, 1, 0);
+  struct sembuf sb;
+  sb.sem_num = 0;
+  //sb.sem_flg = SEM_UNDO;
+  sb.sem_op = -1;
+  printf("trying to get in\n");
+  semop(semd, &sb, 1);
+  //printf("got the semaphore!\n");
+  //sleep(10);
+
+  int shmd;
+  shmd = shmget(SHMKEY, SEGSIZE, 0644);
+  data = shmat(shmd, 0, 0);
+
+  if (!(*data))
+    printf("Last addition:\n");
+  else
+    printf("Last addition: %s\n",data);
+
+  printf("Your addition: ");
+  fgets(data, SEG_SIZE, stdin);
+  *strchr(data, '\n') = 0;
+  printf("%s\n",data);
+
+  file = open("story", O_WRONLY | O_APPEND);
+  write(file, data, SEGSIZE);
+  close(file);
+
+  sb.sem_op = 1;
+  semop(semd, &sb, 1);
+
+  return 0;
+}
